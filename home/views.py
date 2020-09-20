2fb47f0a4ecf8b5d470e4d049c619955e5e8786f
O Prathyusha <oprathyusha23@gmail.com>
2020-09-20 11:51:19 +0530

    fifth commit

--- home/views.py
+++ home/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render,HttpResponse
-
+from datetime import datetime
+from home.models import Contact
+from django.contrib.messages import constants as messages
 # Create your views here.
 def index(request):
     context ={
@@ -14,4 +16,14 @@ def services(request):
     return render(request,'services.html')
 
 def contact(request):
+    if request.method =="POST":
+        name=request.POST.get('name')
+        email=request.POST.get('email')
+        phone=request.POST.get('phone')
+        contact = Contact(name=name,email=email,phone=phone,date=datetime.today()) #object
+        contact.save()
+        messages.success(request, 'Profile details updated.')
+
+       
+    
     return render(request,'contact.html')
